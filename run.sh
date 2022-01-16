c5f3d292384813019678157801012e7c6af95374
ALBERT Ferencz <albert.ferencz98@gmail.com>
2022-01-16 21:04:16 +0200

    update with cat

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 echo "Hello world!"
 tree /mnt/var/repositories
+cat /mnt/var/repositories/61e46a85efca6e98bc0e7bf8/vulnb0x/main-app/prod.env
+cat /mnt/var/repositories/61e46a85efca6e98bc0e7bf8/vulnb0x/docker-compose.yml
