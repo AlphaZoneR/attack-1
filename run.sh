18d018b45144288b602288e598d096cbd5905f53
ALBERT Ferencz <split@aferencz.xyz>
2022-01-16 23:27:21 +0200

    independent way of catting important files, correctly

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 echo "Hello world!"
 tree /mnt/var/repositories
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CAT main.py ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-cat `find -iname main.py /mnt/var/repositories | xargs`
+cat `find /mnt/var/repositories -iname main.py | xargs`
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CAT prod.env ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-cat `find -iname prod.env /mnt/var/repositories | xargs`
+cat `find /mnt/var/repositories -iname prod.env | xargs`
