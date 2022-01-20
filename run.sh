fda425542e91bc9266d41712d141a55fba65d660
ALBERT Ferencz <split@aferencz.xyz>
2022-01-20 17:14:58 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ cat `find /mnt/var/repositories -iname main.py | xargs`
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CAT prod.env ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 cat `find /mnt/var/repositories -iname prod.env | xargs`
 printf \n
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CAT docker-compose.yml ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+cat `find /mnt/var/repositories -iname docker-compose.yml | xargs`
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ List current container ip ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 ping vulnb0x-mongodb -c 1
 ip addr
