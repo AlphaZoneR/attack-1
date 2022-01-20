cddaa389b065994546f6f7b5601f6e451c8a8b91
ALBERT Ferencz <split@aferencz.xyz>
2022-01-20 17:16:26 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CAT main.py ~~~~~~~~~~~~
 cat `find /mnt/var/repositories -iname main.py | xargs`
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CAT prod.env ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 cat `find /mnt/var/repositories -iname prod.env | xargs`
-printf \n
+printf "\n"
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CAT docker-compose.yml ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 cat `find /mnt/var/repositories -iname docker-compose.yml | xargs`
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ List current container ip ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
